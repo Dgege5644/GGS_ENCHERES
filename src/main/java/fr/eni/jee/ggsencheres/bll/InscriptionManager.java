ba08e22a3ae756e0dbd4b4acd9b3775d73e13caf
gchatte2021 <gchatte2021@27SE21-45NZG5J.ad.campus-eni.fr>
2021-08-20 17:22:47 +0200

    GG vendredi soir Inscription fonctionne sauf sur message et redirection JSP

--- src/main/java/fr/eni/jee/ggsencheres/bll/InscriptionManager.java
+++ src/main/java/fr/eni/jee/ggsencheres/bll/InscriptionManager.java
@@ -12,8 +12,14 @@ public class InscriptionManager {
 	
 	private UtilisateurDAO utilisateurDAO;
 	private Utilisateur userAcreer;
-	private boolean validation;
-	private boolean validationNom;
+	
+	
+	
+	
+	
+	
+	
+	 
 	private List<Utilisateur> listeUtilisateurs;
 	
 	
@@ -21,6 +27,18 @@ public class InscriptionManager {
         this.utilisateurDAO = DAOFactory.getUtilisateurDAO(); // utilisateurDAO représente la DAL
     }
 	
+	
+	public Utilisateur addSansVerif (String pseudo, String nom, String prenom, String email, String telephone, String rue,
+			String codePostal, String ville, String motDePasse) throws BLLException {
+		Utilisateur userNonVerifie = null;
+		try {
+			userNonVerifie = this.utilisateurDAO.addUtilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return userNonVerifie;
+	}
 
 	/**
 	* méthode qui valide l'inscription en utilisant une méthode de validation par champ et 
@@ -29,44 +47,23 @@ public class InscriptionManager {
 	 * @throws DALException 
 	*/
 	
-	public Utilisateur validerNouvelUtilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue,
-			String codePostal, String ville, String motDePasse) throws BLLException {
+	public Utilisateur creerNouvelUtilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue,
+			String codePostal, String ville, String motDePasse, String confirmation) throws BLLException {
 		try {
-			// Si toutes les méthodes appelées retournent true...
-		// appelle la méthode qui valide le pseudo
-			if (this.validerPseudo(pseudo)&& 		
-		// appelle la méthode qui valide le nom
-			validationNom == true &&
-		
-		// appelle la méthode qui valide le prenom
-			this.validerPrenom(prenom) &&
+			// Si un des méthodes appelées retourne false ..
+			if (validerPseudo(pseudo) == false || validerEmail(email) == false || validerMotDePasse(motDePasse, confirmation) == false) {
+					
+				// on lève une BLLException
+				throw new BLLException("il y a une erreur dans un des champs saisis");
+			}
+			// Sinon on ajoute le userAcreer en BDD avec les paramètres récupérés du formulaire de saisie
+			userAcreer=this.utilisateurDAO.addUtilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse);
+				
 			
-		// appelle la méthode qui valide l'email
-			this.validerEmail(email) &&
-		
-		// appelle la méthode qui valide le telephone
-			this.validerTelephone(telephone) &&
-		
-		// appelle la méthode qui valide la rue
-			this.validerRue(rue) &&
-		
-		// appelle la méthode qui valide le codePostal
-			this.validerCodePostal(codePostal) &&
-		
-		// appelle la méthode qui valide la ville
-			this.validerVille(ville) &&
-		
-		// appelle la méthode qui valide le mot de passe
-			this.validerMotDePasse(motDePasse)) {
-			// On ajoute le userAcreer en BDD
-			userAcreer=this.utilisateurDAO.addUtilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse)
-					;
-			}	
-			// Sinon on lève une BLLException
 		} catch (DALException e) {
 			throw new BLLException(e.getMessage());
 		}
-		// Quoi qu'il en soit on retourne l'userAcreer
+		// Quoi qu'il en soit on retourne l'userAcreer (qui est soit null soit créé avec les infos entrées en paramètres)
 		return userAcreer;
 	}
 
@@ -76,140 +73,167 @@ public class InscriptionManager {
 	 * @return
 	 * @throws BLLException
 	 */
-	private boolean validerPseudo(String pseudo) throws BLLException {
-		
+	public boolean validerPseudo(String pseudo) throws BLLException {
+		boolean validationPseudo=false;
 		try {
+			
 			//Pour valider le 1er point (n'existe pas déjà en base), il faut parcourir 
 			//les pseudos de toute la base de données ==> boucle forEach
-			for (Utilisateur u : this.listeUtilisateurs)
-				//le pseudo est validé si :
-				//1- n'est pas déjà présent dans la base de données parmi l'ensemble des utilisateurs
-				//2- ET n'est pas null
+			//for (Utilisateur u : this.listeUtilisateurs)
+				Utilisateur u = utilisateurDAO.getInfosUtilisateur(pseudo);
+				//le pseudo ne peut etre validé si :
+				//1- il est nul n'est pas déjà présent dans la base de données parmi l'ensemble des utilisateurs
+				//2- ou 
 				//3- ET est alphanumérique
-				if (pseudo != utilisateurDAO.getInfosUtilisateur(pseudo).getPseudo() && pseudo!=null && pseudo.matches("[A-Za-z0-9]+")){
-					validation = true;
+				
+				//TODO vérifier que c'est en alphanumérique
+				if (pseudo == null || u != null||  !pseudo.matches("^[a-zA-Z0-9]+$")){
+					throw new BLLException("le pseudo n'est pas valide");
 			}
+				validationPseudo = true;
 		} catch (Exception e){
-				validation = false;
-				throw new BLLException(e.getMessage());
+			throw new BLLException(e.getMessage());
 		}
-			return validation;
+			return validationPseudo;
 	}
 	
-	
-	
-	private boolean validerNom(String nom) throws BLLException {
+	public boolean validerEmail(String email)throws BLLException {
+		// c'est OK si non nul, alphanumérique et contient un @ 
+		//TODO vérifier que c'est en alphanumérique + @
+		boolean validationEmail = false;
+		
 		try {
-			if (nom!= null && nom.matches("[A-Za-z]")) {
-				validationNom = true;
-			}
+			Utilisateur u = utilisateurDAO.getInfosUtilisateur(email);
+			if (email== null || u!=null)/* || !email.matches("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b"))*/ {
+				throw new BLLException("le nom n'est pas valide");
+				
+			} validationEmail = true;
 		} catch (Exception e) {
-				validationNom = false;
+			
+			
+			throw new BLLException(e.getMessage());
+
+		} return validationEmail;
+	}
+	
+	public boolean validerMotDePasse(String motDePasse, String confirmation)throws BLLException {
+		// c'est OK si c'est non déjà présent en BDD, non nul et alphanumérique
+		//TODO vérifier que c'est en alphanumérique
+		boolean validationMotDePasse;
+		try {
+					
+			if (motDePasse== null || !motDePasse.equals(confirmation)) {
+					throw new BLLException("le mot de passe n'est pas valide");
+					
+			} validationMotDePasse = true;
+		} catch (Exception e) {
+			
+			throw new BLLException(e.getMessage());
+
+		} return validationMotDePasse;
+	}
+	
+	public boolean validerNom(String nom) throws BLLException {
+		boolean validationNom = false;
+		
+		try {
+			//TODO vérifier que c'est en alphanumérique
+			if (nom == null || !nom.matches("^[a-zA-Z0-9]+$")) {
+				throw new BLLException("le nom n'est pas valide");
+				
+			}validationNom = true;
+			
+		} catch (Exception e) {
+				
 				throw new BLLException("erreurNom"+ e.getMessage());
 	
 		} return validationNom;
 	}
 	
-	private boolean validerPrenom(String prenom) throws BLLException {
+	public boolean validerPrenom(String prenom) throws BLLException {
+		boolean validationPrenom = false;
 		try {
-			if (prenom != null && prenom.matches("[A-Za-z0-9]+")) {
-				validation = true;
-			}
+			//TODO vérifier que c'est en alphanumérique
+			if (prenom == null)/* || !prenom.matches("[A-Za-z0-9]+"))*/ {
+				throw new BLLException("le nom n'est pas valide");
+				
+			} validationPrenom = true;
+			
 		} catch (Exception e) {
-			validation = false;
 			throw new BLLException(e.getMessage());
 
-		} return validation;
+		} return validationPrenom;
 		
 	}
 	
-	private boolean validerEmail(String email)throws BLLException {
-		// c'est OK si non nul, alphanumérique et contient un @ 
-		//TODO vérifier qu'il y a un @
-		try {
-			if (email!= null && email.matches("[A-Za-z0-9]+")) {
-				validation = true;
-			}
-		} catch (Exception e) {
-			validation = false;
-			throw new BLLException(e.getMessage());
-
-		} return validation;
-	}
 	
-	private boolean validerTelephone(String telephone)throws BLLException {
+	public boolean validerTelephone(String telephone)throws BLLException {
 		// c'est OK si c'est nul ou si ce sont des chiffres
+		//TODO vérifier que c'est en numérique
+		boolean validationTelephone;
 		try {
-			if (telephone.matches("[0-9]+")) {
-				validation = true;
-			}
+			if (!telephone.matches("[0-9]")) {
+				throw new BLLException("le téléphone n'est pas valide");
+				
+			} validationTelephone = true;
 		} catch (Exception e) {
-			validation = false;
+			//throw new BLLException(e.getMessage());
 			throw new BLLException(e.getMessage());
 
-		} return validation;
+		} return validationTelephone;
 	}
 	
-	private boolean validerRue(String rue) throws BLLException {
+	public boolean validerRue(String rue) throws BLLException {
 		// C'est OK si c'est non nul et alphanumérique + espaces
+		//TODO vérifier que c'est en alphanumérique
+		boolean validationRue = false;
 		try {
-			if (rue!= null && rue.matches("[A-Za-z0-9]+")) {
-				validation = true;
-			}
+			if (rue == null)/* || rue.matches("[A-Za-z0-9]+"))*/ {
+				throw new BLLException("la rue n'est pas valide");
+				
+			} validationRue = true;
 		} catch (Exception e) {
-			validation = false;
+			
 			throw new BLLException(e.getMessage());
 
-		} return validation;
+		} return validationRue;
 	}
 	
-	private boolean validerCodePostal(String codePostal)throws BLLException {
+	public boolean validerCodePostal(String codePostal)throws BLLException {
 		// C'est Ok si c'est non nul,  que ce sont des chiffres et que la taille fait 5
+		//TODO vérifier que c'est en numérique
+		boolean validationCodePostal = false;
+		
 		try {
-			if (codePostal!= null && codePostal.matches("[0-9]+")&& codePostal.length()==5) {
-				validation = true;
-			}
+			if (codePostal == null || codePostal.length()!=5)/* || !codePostal.matches("[0-9]+"))*/ {
+				throw new BLLException("le code postal n'est pas valide");
+				
+			} validationCodePostal = true;
 		} catch (Exception e) {
-			validation = false;
+			
 			throw new BLLException(e.getMessage());
 
-		} return validation;
+		} return validationCodePostal;
 	}
 	
-	private boolean validerVille(String ville)throws BLLException {
+	public boolean validerVille(String ville)throws BLLException {
 		// c'est OK si c'est non nul et en alphanumérique
-		try {
-			if (ville!= null && ville.matches("[A-Za-z0-9]+")) {
-				validation = true;
-			}
-		} catch (Exception e) {
-			validation = false;
-			throw new BLLException(e.getMessage());
-
-		} return validation;
-	}
-	
-	private boolean validerMotDePasse(String motDePasse)throws BLLException {
-		// c'est OK si c'est non déjà présent en BDD, non nul et alphanumérique 	
-		try {
-			for (Utilisateur u : this.listeUtilisateurs)
-				if (motDePasse != utilisateurDAO.getInfosUtilisateur(motDePasse).getMotDePasse() && motDePasse!= null && motDePasse.matches("[A-Za-z0-9]+")) {
-				validation = true;
-			}
-		} catch (Exception e) {
-			validation = false;
-			throw new BLLException(e.getMessage());
-
-		} return validation;
-	}
-	
-	private List<Utilisateur> getListeUtilisateurs() throws BLLException {
+		//TODO vérifier que c'est en alphanumérique
+		boolean validationVille = false;
 		
 		try {
-			this.listeUtilisateurs = utilisateurDAO.selectAllUtilisateurs();
-		} catch (DALException e) {
+			if (ville == null)/* || !ville.matches("[A-Za-z0-9]+"))*/ {
+				throw new BLLException("la ville n'est pas valide");
+				
+			} validationVille = true;
+		} catch (Exception e) {
+			
 			throw new BLLException(e.getMessage());
-		}return this.listeUtilisateurs;
-		
+
+		} return validationVille;
 	}
+	
+	
+	
+	
 }
